97141caa88eca44731296becbdad4002275327c9
Vasileios Papastergios <papastva@csd.auth.gr>
2020-11-08 00:08:58 +0200

    Renamed getName method to printDescription.

--- src/RoundI.java
+++ src/RoundI.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
 public interface RoundI {
-    String getName();
+    void printDescription();
     int getNumberOfQuestions();
     ArrayList<PlayerTest> getPlayers();
+
     void askQuestion();
     String readAnswer();
     void giveCredits(String givenAnswer);
