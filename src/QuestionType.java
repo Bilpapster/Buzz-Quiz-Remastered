7ec168236d2b683b6d9de7e7ec343baf065d7942
fmalakis <xan.klem.30@gmail.com>
2020-11-07 21:08:58 +0200

    Added an extra QuestionType for 'Geography'

--- src/QuestionType.java
+++ src/QuestionType.java
@@ -2,5 +2,5 @@
  * Class containing all the possible types for the questions
  */
 public enum QuestionType {
-    History, Movies_and_Series, Science, Sports, Technology, Music, Food_and_Culture
+    History, Movies_and_Series, Science, Sports, Technology, Music, Food_and_Culture, Geography
 }
