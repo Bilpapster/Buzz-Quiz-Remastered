214fc4312a9fd7705e9d6fd419a3795e39d90983
fmalakis <xan.klem.30@gmail.com>
2020-11-07 18:29:10 +0200

    Added extra question type

--- src/QuestionType.java
+++ src/QuestionType.java
@@ -2,5 +2,5 @@
  * Class containing all the possible types for the questions
  */
 public enum QuestionType {
-    History, Movies_and_Series, Science, Sports, Technology, Music
+    History, Movies_and_Series, Science, Sports, Technology, Music, Food_and_Culture
 }
