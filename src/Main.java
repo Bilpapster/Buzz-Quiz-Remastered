f02aa83bfad54d4e8b1c1ddb4683b7257777304e
Vasileios Papastergios <papastva@csd.auth.gr>
2020-11-07 11:49:03 +0200

    Added Parser and PlayerTest classes. Made a few adjustments to main function.

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         QuestionManager questionManager = new QuestionManager();
+        Parser parser = new Parser();
 
         questionManager.createQuestions();
 
-        System.out.print("State your name player: ");
-        Player player1 = new Player(input.nextLine());
+        PlayerTest player1 = new PlayerTest(parser.askForName());
 
         while(questionManager.questionsRemaining() > 0) { // temporary way of knowing when the game will end, will be changed to rounds later on
             System.out.println();
-            System.out.println();
             player1.printScore();
             questionManager.getNextQuestion().displayQuestion();
-            String ans = input.nextLine();
+            String ans = parser.askForAnswer();
             if (questionManager.getNextQuestion().isCorrectAnswer(ans)) {
                 System.out.println("Correct!");
                 player1.updateScore(1000);
---
+++ src/Parser.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+/**
+ * A simple class representing a parsing object in our game.
+ * Objects of this class serve on communicating with user via console.
+ */
+public class Parser {
+    private Scanner inputScanner;
+
+    /**
+     * Creates a parsing object for communication via the default input stream
+     */
+    public Parser() {
+        this.inputScanner = new Scanner(System.in);
+    }
+
+    /**
+     * Shows a helping message to user. Asks for the player's name.
+     *
+     * @return the players name given by user
+     */
+    public String askForName() {
+        System.out.print("State your name player: ");
+        return inputScanner.nextLine();
+    }
+
+    /**
+     * Shows a helping message to user. Asks for their answer.
+     * Does answer validation check, so that it is one of the acceptable ones.
+     *
+     * @return a valid answer given by user
+     */
+    public String askForAnswer() {
+        System.out.print("Your answer: ");
+        String givenAnswer = inputScanner.nextLine();
+
+        while (!isValidAnswer(givenAnswer)) {
+            System.out.print("Invalid answer. Valid options: [a, b, c, d]. Your answer: ");
+            givenAnswer = inputScanner.nextLine();
+        }
+        return givenAnswer;
+    }
+
+    /**
+     * Checks whether a given answer is valid or not. The code can become a lot smarter.
+     *
+     * @param givenAnswer a string to check its validity as an answer
+     * @return true if the string is a valid answer, else false.
+     */
+    public Boolean isValidAnswer(String givenAnswer) {
+        if (givenAnswer.length() > 1) {
+            return false;
+        }
+
+        if (!(givenAnswer.equals("a") || givenAnswer.equals("b")
+                || givenAnswer.equals("c") || givenAnswer.equals("d"))) {
+            return false;
+        }
+        return true;
+    }
+}
