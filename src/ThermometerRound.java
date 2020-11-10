d5fd052109a52ade77094db46d82c1ff40590a2d
Vasileios Papastergios <papastva@csd.auth.gr>
2020-11-11 00:03:45 +0200

    Initial commit and basic code for ThermometerRound class, as a sub-class of StandardRound. Java doc in missing, yet coming soon.

---
+++ src/ThermometerRound.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ThermometerRound extends StandardRound {
+
+    private HashMap<Player, Integer> correctAnswersInRound;
+    private Boolean winnerFound;
+    private int winningScore;
+
+    public ThermometerRound(ArrayList<Player> players, QuestionManager questionManager, Parser parser) {
+        super(0, players, questionManager, parser);
+        this.creditPoints = 5000;
+        this.winnerFound = false;
+        this.winningScore = 5;
+        correctAnswersInRound = new HashMap<>();
+
+        for (Player player : players) {
+            correctAnswersInRound.put(player, 0);
+        }
+    }
+
+    /**
+     * Printing method for the round description. Written hard coded TEMPORARILY.
+     */
+    @Override
+    public void printDescription() {
+        System.out.printf("In this round you are going to be asked " + this.getNumberOfQuestionsRemaining() + " questions.%n"
+                + "For every correct answer, you earn " + this.getCreditPoints() + " points!%n"
+                + "Press enter to start round ");
+        parser.getEnter();
+    }
+
+    @Override
+    public Boolean isOver() {
+        return this.winnerFound;
+    }
+
+    @Override
+    protected void executeActionsOnCorrectAnswer(Player player) {
+        System.out.print(player.getName() + ": Correct!");
+        addOneCorrectAnswerTo(player);
+        System.out.println();
+    }
+
+    @Override
+    protected void executeActionsOnWrongAnswer(Player player) {
+        System.out.print(player.getName() + ": Wrong...");
+        System.out.println();
+    }
+
+    @Override
+    protected void executeActionsOnEndOfQuestion() {
+        HashSet<Player> playersReachedWinningZone = new HashSet<>();
+        for (Player player : players) {
+            if (correctAnswersInRound.get(player) == winningScore) {
+                playersReachedWinningZone.add(player);
+            }
+        }
+
+        if (playersReachedWinningZone.size() == 1) {
+            for (Player player : playersReachedWinningZone) {
+                System.out.println(player.getName() + " is the winner of the round! +" + getCreditPoints());
+                player.updateScore(creditPoints);
+                winnerFound = true;
+            }
+        } else if (playersReachedWinningZone.size() > 1) {
+            players.clear();
+            players.addAll(playersReachedWinningZone);
+            winningScore++;
+        }
+        questionManager.removeAnsweredQuestion();
+    }
+
+    private void addOneCorrectAnswerTo(Player player) {
+        correctAnswersInRound.put(player, correctAnswersInRound.get(player) + 1);
+    }
+}
