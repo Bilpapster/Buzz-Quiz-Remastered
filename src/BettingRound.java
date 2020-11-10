6cb0045e1d816016440ed7e055798f8e2b47a133
Vasileios Papastergios <papastva@csd.auth.gr>
2020-11-10 10:09:15 +0200

    Converted HashSet to TreeMap for enhanced performance and order of stored keys. Added java doc for class and methods.

--- src/BettingRound.java
+++ src/BettingRound.java
@@ -1,42 +1,67 @@
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.TreeMap;
 
+/**
+ * A class representing a specific type of round, where the players place a bet for each question.
+ * The class is implemented as sub-class of the StandardRound class.
+ */
 public class BettingRound extends StandardRound {
-    private Set<String> acceptableBets;
+    private TreeMap<String, Integer> acceptableBets;
 
+    /**
+     * Constructs a BettingRound object with given attributes.
+     *
+     * @param numberOfQuestions the number of questions in the round
+     * @param players           the players involved in the round
+     * @param questionManager   a question-managing object, responsible for the questions of the round
+     * @param parser            a parsing object responsible for communicating with player(s) via console
+     */
     public BettingRound(int numberOfQuestions, ArrayList<Player> players, QuestionManager questionManager, Parser parser) {
         super(numberOfQuestions, players, questionManager, parser);
         initializeAcceptableBetsSet();
     }
 
+    /**
+     * Initializes the acceptable bets as a map structure.
+     */
     private void initializeAcceptableBetsSet() {
-        this.acceptableBets = new HashSet<String>();
-        acceptableBets.add("250");
-        acceptableBets.add("500");
-        acceptableBets.add("750");
-        acceptableBets.add("1000");
+        this.acceptableBets = new TreeMap<String, Integer>();
+        acceptableBets.put("250", 250);
+        acceptableBets.put("500", 500);
+        acceptableBets.put("750", 750);
+        acceptableBets.put("1000", 1000);
     }
 
+    /**
+     * Prints the round description. Asks for the player to press enter.
+     */
     @Override
     public void printDescription() {
         System.out.printf("In this round you are going to be asked " + this.getNumberOfQuestions() + " questions.%n"
-                + "At first, you are let to know the category of the question and you are asked to place a bet " + acceptableBets.toString() +
+                + "At first, you are let to know the category of the question and you are asked to place a bet " + acceptableBets.keySet() +
                 " Answering correctly will add to your score as many points as your bet!%n" +
                 " But be careful! Answering the question wrong will cost you your bet!%n"
                 + "Press enter to start round ");
         parser.getEnter();
     }
 
+    /**
+     * Displays the question to player(s). At first, the question category is displayed and the player(s) are asked to place a bet.
+     * After the bet placement, the question itself and the available answers are displayed simultaneously.
+     */
     @Override
     public void askQuestion() {
         System.out.println();
         questionManager.getNextQuestion().displayCategory();
-        this.setPointsEarnedOnCorrectAnswer(Integer.parseInt(parser.askForBet(acceptableBets)));
+        this.setPointsEarnedOnCorrectAnswer(Integer.parseInt(parser.askForBet(acceptableBets.keySet())));
         questionManager.getNextQuestion().displayQuestionBody();
         questionManager.getNextQuestion().displayOptions();
     }
 
+    /**
+     *
+     * @param givenAnswer the answer to compare against the correct one, in order to give credits or not
+     */
     @Override
     public void giveCredits(String givenAnswer) {
         if (questionManager.getNextQuestion().isCorrectAnswer(givenAnswer)) {
@@ -46,7 +71,7 @@ public class BettingRound extends StandardRound {
             }
         } else {
             for (Player player : players) {
-                System.out.println("Wrong...! - "+ getPointsEarnedOnCorrectAnswer() + " points!");
+                System.out.println("Wrong...! - " + getPointsEarnedOnCorrectAnswer() + " points!");
                 player.updateScore(getPointsEarnedOnCorrectAnswer() * (-1));
             }
         }
