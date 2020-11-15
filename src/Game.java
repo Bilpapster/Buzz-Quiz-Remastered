de3da86dc28c06fc89ad9b2e5d01958300564444
Vasileios Papastergios <papastva@csd.auth.gr>
2020-11-15 19:08:00 +0200

    Added a new constructor for possible use later, on GUI design. Also, changed the way of round selection to random, by adding a switch case block inside initializeGamePlay() method.

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,32 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
-    private ArrayList<Player> players;
-    private ArrayList<RoundI> rounds;
-    private QuestionManager questionManager;
-    private Parser parser;
-    private HighscoreManager highscoreManager;
+    private final int numberOfRounds;
+    private final int numberOfQuestionsPerRound;
+    private final ArrayList<Player> players;
+    private final ArrayList<RoundI> rounds;
+    private final QuestionManager questionManager;
+    private final Parser parser;
+    private final HighscoreManager highscoreManager;
 
     public Game() {
+        numberOfRounds = 6;
+        numberOfQuestionsPerRound = 5;
         players = new ArrayList<>();
+        rounds = new ArrayList<>();
+        questionManager = new QuestionManager();
+        questionManager.createQuestions();
+        parser = new Parser();
+        highscoreManager = new HighscoreManager();
+        players.add(new Player());
+        players.add(new Player());
+    }
+
+    public Game(int numberOfRounds, int numberOfQuestionsPerRound, ArrayList<Player> players) {
+        this.numberOfRounds = numberOfRounds;
+        this.numberOfQuestionsPerRound = numberOfQuestionsPerRound;
+        this.players = players;
         rounds = new ArrayList<>();
         questionManager = new QuestionManager();
         questionManager.createQuestions();
@@ -16,21 +34,29 @@ public class Game {
         highscoreManager = new HighscoreManager();
     }
 
-    public Game(ArrayList<Player> players, ArrayList<RoundI> rounds, Parser parser) {
-        this.players = players;
-        this.rounds = rounds;
-        this.parser = parser;
-    }
-
     public void initializeGamePlay() {
-        players.add(new Player());
-//        players.add(new Player());
-//        players.add(new Player());
-        rounds.add(new StandardRound(1, players, questionManager, parser));
-        rounds.add(new BettingRound(1, players, questionManager, parser));
-        rounds.add(new StopTheClockRound(1, players, questionManager, parser));
-        rounds.add(new QuickAnswerRound(1, players, questionManager, parser));
-        rounds.add(new ThermometerRound(players, questionManager, parser));
+
+        Random randomNumbersGenerator = new Random(System.currentTimeMillis());
+
+        for (int round = 0; round < numberOfRounds; round++) {
+            int randomNumber = (Math.abs(randomNumbersGenerator.nextInt()) % 5) + 1;
+            switch (randomNumber) {
+                case 2:
+                    rounds.add(new BettingRound(numberOfQuestionsPerRound, players, questionManager, parser));
+                    break;
+                case 3:
+                    rounds.add(new StopTheClockRound(numberOfQuestionsPerRound, players, questionManager, parser));
+                    break;
+                case 4:
+                    rounds.add(new QuickAnswerRound(numberOfQuestionsPerRound, players, questionManager, parser));
+                    break;
+                case 5:
+                    rounds.add(new ThermometerRound(players, questionManager, parser));
+                    break;
+                default:
+                    rounds.add(new StandardRound(numberOfQuestionsPerRound, players, questionManager, parser));
+            }
+        }
     }
 
     public void play() {
@@ -38,10 +64,10 @@ public class Game {
         for (RoundI round : rounds) {
             System.out.println();
             System.out.println("**********" + " Round " + roundsCounter + " **********");
-            System.out.printf(round.getDescription());
+            System.out.print(round.getDescription());
             System.out.print("Press enter to start round ");
             parser.getEnter();
-            while (! round.isOver()) {
+            while (!round.isOver()) {
                 round.askQuestion();
                 round.readAnswers();
                 round.giveCredits();
