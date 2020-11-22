63651fe5a03e2c216fc1d26b9b76fd90cb4cf8dc
fmalakis <xan.klem.30@gmail.com>
2020-11-22 23:53:33 +0200

    Added certain methods for accesing the timer component (just for testing, can be removed for the production version)

--- src/GUI/GameWindow.java
+++ src/GUI/GameWindow.java
@@ -7,19 +7,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.*;
 
 public class GameWindow implements ActionListener {
 
     ArrayList<Player> listOfPlayers;
     Question currentQuestion;
+    TimerComponent timer;
     JFrame gameFrame = new JFrame();
     JLabel questionText = new JLabel();
     JPanel questionPanel = new JPanel();
     JPanel answersPanel = new JPanel();
+    JPanel footerPanel = new JPanel();
+    int indexLOL = 0;
     QuestionManager questionManager = new QuestionManager();
     ArrayList<JButton> answerBtns = new ArrayList<>();
 
+    // TODO: really need to break down this constructor to smaller functions
     public GameWindow(ArrayList<Player> listOfPlayers) {
         this.listOfPlayers = listOfPlayers;
         questionManager.createQuestions();
@@ -27,7 +33,7 @@ public class GameWindow implements ActionListener {
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.setSize(1280, 720);
         gameFrame.setLayout(new BorderLayout());
-        gameFrame.getContentPane().setBackground(Color.darkGray);
+        gameFrame.setBackground(Color.darkGray);
         gameFrame.setLocationRelativeTo(null);
         gameFrame.setVisible(true);
 
@@ -41,6 +47,15 @@ public class GameWindow implements ActionListener {
 
 
         gameFrame.add(answersPanel, BorderLayout.CENTER);
+        setUpFooter();
+
+
+        gameFrame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(!timer.isOver()? "The timer is still going!" : "The timer has finished!");
+            }
+        });
 
         prepareNextQuestion();
 
@@ -71,6 +86,60 @@ public class GameWindow implements ActionListener {
 
         questionManager.removeAnsweredQuestion();
 
+        // this is for testing puproses, remove for actual production version
+        if (indexLOL % 2 == 0) {
+            addTimerComponent();
+            showTimerComponent();
+            showGetReadyMessage();
+        }
+
+        indexLOL++;
+        // ------------------------------------------------------------------
+    }
+
+    /**
+     * This method generates a new timer component and adds it to the
+     * footer panel
+     */
+    private void addTimerComponent() {
+        timer = new TimerComponent();
+        footerPanel.add(timer.getMainPanel());
+    }
+
+    /**
+     * Sets up the footer of the page which is going to house
+     * the timer component, then hides it (can also house more components
+     * in the future as needed)
+     */
+    private void setUpFooter() {
+        footerPanel.setBackground(Color.DARK_GRAY);
+        footerPanel.setVisible(false);
+        gameFrame.add(footerPanel, BorderLayout.SOUTH);
+    }
+
+    /**
+     * Shows the footer of the page containing the timer
+     */
+    private void showTimerComponent() {
+        footerPanel.setVisible(true);
+    }
+
+    /**
+     * Hides the footer of the page containing the timer, and
+     * clears it from the current timer object
+     */
+    private void hideTimerComponent() {
+        footerPanel.setVisible(false);
+        footerPanel.removeAll();
+    }
+
+    /**
+     * This method displays an information message before a timed round begins, after which,
+     * uppon being dismissed, starts the timer for the question.
+     */
+    private void showGetReadyMessage() {
+        JOptionPane.showMessageDialog(null, "Timed Round! Get ready to race for points!");
+        timer.startTimer();
     }
 
     private void checkIfCorrectAnswer(int index) {
@@ -83,7 +152,9 @@ public class GameWindow implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        timer.stopTimer();
         checkIfCorrectAnswer(answerBtns.indexOf(e.getSource()));
+        hideTimerComponent();
         prepareNextQuestion();
     }
 }
