0095925ece46d4b76c5e6d8e0b16d62a7dbde1e9
fmalakis <xan.klem.30@gmail.com>
2020-11-22 23:52:30 +0200

    Added Timer component to be used with the TimedRound

---
+++ src/GUI/TimerComponent.java
@@ -0,0 +1,169 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+
+public class TimerComponent {
+
+    private JPanel panel;
+    JLabel title;
+    JLabel timerLabel;
+    JLabel timeOverLabel;
+    SimpleDateFormat df = new SimpleDateFormat("mm:ss:SSS");
+    private boolean isOver = false;
+    private Timer timer;
+    private long startTime = -1;
+    private long duration = 5000;
+
+    /**
+     * Initializes the timer component, by calling the <code>setUpPanel()</code> function
+     * which handles the GUI building
+     */
+    public TimerComponent() {
+        setUpPanel();
+    }
+
+    /**
+     * Sets up the main panel
+     */
+    private void setUpPanel() {
+        panel = new JPanel();
+
+        panel.setLayout(new BorderLayout());
+        panel.setBounds(180,180,500,250);
+        panel.setBackground(Color.darkGray);
+
+
+        setUpHeader();
+        setUpTimerLabel();
+        setUpTimer();
+        setUpFooter();
+
+        panel.setVisible(true);
+    }
+
+    /**
+     * Sets up the header of the component
+     */
+    public void setUpHeader() {
+        title = new JLabel();
+        title.setText("Timed Round!");
+        title.setForeground(Color.WHITE);
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setFont(new Font("Arial Black", Font.BOLD, 24));
+        panel.add(title, BorderLayout.NORTH);
+    }
+
+    /**
+     * Sets up the JLabel that will be displaying how much time is left
+     * in real time
+     */
+    public void setUpTimerLabel() {
+        timerLabel = new JLabel();
+        timerLabel.setText(df.format(duration));
+        timerLabel.setFont(new Font("Arial Black", Font.ITALIC, 28));
+        timerLabel.setForeground(Color.ORANGE);
+        timerLabel.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(timerLabel, BorderLayout.CENTER);
+    }
+
+    /**
+     * Sets up a new Timer object with a duration of 5000 milliseconds,
+     * and awaits for the start of it, uppon which it constantly ticks down
+     * until it reaches zero.
+     */
+    private void setUpTimer() {
+        timer = new Timer(10, e -> {
+            if (startTime < 0) {
+                startTime = System.currentTimeMillis();
+            }
+            long now = System.currentTimeMillis();
+            long clockTime = now - startTime;
+            if (clockTime >= duration) {
+                clockTime = duration;
+                timeOverLabel.setVisible(true);
+                isOver = true;
+                timer.stop();
+            }
+            timerLabel.setText(df.format(duration - clockTime));
+        });
+        timer.setInitialDelay(0);
+    }
+
+    /**
+     * Starts the timer
+     */
+    public void startTimer() {
+        if (!timer.isRunning()) {
+            startTime = -1;
+            timer.start();
+        }
+    }
+
+    /**
+     * Returns the main panel of the component
+     * @return a <code>JPanel</code> which contains all elements of TimeComponent
+     */
+    public JPanel getMainPanel() {
+        return panel;
+    }
+
+    /**
+     * Method which returns wether the timer has reached 0 or not
+     * @return returns <code>true</code> if the timer has reached 0, otherwise
+     * returns <code>false</code>
+     */
+    public boolean isOver() {
+        return isOver;
+    }
+
+    /**
+     * Stops the timer
+     */
+    public void stopTimer() {
+        timer.stop();
+    }
+
+    /**
+     * Hides the TimerComponent panel
+     * @return <code>true</code> if the panel was previously shown and it has been hidden, or <code>false</code>
+     * if the panel was already hidden.
+     */
+    public boolean hideTimer() {
+        if (panel.isVisible()) {
+            panel.setVisible(false);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Shows the TimerComponent panel
+     * @return <code>true</code> if the panel was previously hidden and it has been shown, or <code>false</code>
+     * if the panel was already shown.
+     */
+    public boolean showTimer() {
+        if (!panel.isVisible()) {
+            panel.setVisible(true);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Sets up the footer of the panel which gets displayed as soon as the
+     * timer reaches 0
+     */
+    private void setUpFooter() {
+        timeOverLabel = new JLabel();
+        timeOverLabel.setForeground(Color.white);
+        timeOverLabel.setText("<html>Time has <font color=red><b>run out</b></font>! No bonus will be handed out for this round!</html>");
+        timeOverLabel.setVisible(false);
+        timeOverLabel.setFont(new Font("Arial Black", Font.PLAIN, 18));
+        timeOverLabel.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(timeOverLabel, BorderLayout.SOUTH);
+    }
+
+
+}
