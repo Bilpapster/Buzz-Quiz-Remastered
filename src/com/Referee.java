72bdb7fbde8766d19dec42cdeb9f5bd4f3c7ec9e
Vasileios Papastergios <papastva@csd.auth.gr>
2020-12-24 15:34:49 +0200

    Initial commit for the Referee class. Referee is going to have key-role in model-view controlling.

---
+++ src/com/Referee.java
@@ -0,0 +1,93 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Referee {
+    private QuestionManager questionManager;
+    private Question currentQuestion;
+    private ArrayList<Player> playersInGame;
+    private ArrayList<Player> alivePlayersInRound;
+    private Boolean playersNeedToBeReset;
+    private HashMap<Player, Long> milliSecondsElapsedOnAnswer;
+    private HashMap<Player, String> answersGivenByPlayers;    // the players' answers to the current question
+
+    public Referee(ArrayList<Player> playersInGame) {
+        this.questionManager = new QuestionManager();
+        questionManager.createQuestions();
+        currentQuestion = questionManager.getNextQuestion();
+
+        this.playersInGame = playersInGame;
+        this.alivePlayersInRound = new ArrayList<>();
+        alivePlayersInRound.addAll(playersInGame);
+
+        playersNeedToBeReset = false;
+
+        milliSecondsElapsedOnAnswer = new HashMap<>();
+        answersGivenByPlayers = new HashMap<>();
+    }
+
+    public void setAnswerData(Player player, String answer, long timeElapsedOnAnswer) {
+        answersGivenByPlayers.put(player, answer);
+        milliSecondsElapsedOnAnswer.put(player, timeElapsedOnAnswer);
+    }
+
+    public void setAnswerData(Player player, String answer) {
+        this.setAnswerData(player, answer, -1);
+    }
+
+    private void resetAllAnswerData() {
+        answersGivenByPlayers.clear();
+        milliSecondsElapsedOnAnswer.clear();
+    }
+
+    private void resetAllRoundData() {
+        if (playersNeedToBeReset) {
+            alivePlayersInRound.clear();
+            alivePlayersInRound.addAll(playersInGame);
+            playersNeedToBeReset = false;
+        }
+        resetAllAnswerData();
+    }
+
+    public void removeFromAlivePlayers(Player playerToRemove) {
+        alivePlayersInRound.remove(playerToRemove);
+        this.playersNeedToBeReset = true;
+    }
+
+    public ArrayList<Player> getAlivePlayersInRound() {
+        return alivePlayersInRound;
+        /*
+        ArrayList<Player> defensiveCopy = new ArrayList<>();
+        defensiveCopy.addAll(alivePlayersInRound);
+        return defensiveCopy;
+         */
+    }
+
+    public Boolean hasPlayerAnsweredCorrectly(Player player) {
+        return answersGivenByPlayers.get(player).equals(currentQuestion.getCorrectAnswer());
+    }
+
+    public Long getTimeElapsedOnAnswerForPlayer(Player player) {
+        return milliSecondsElapsedOnAnswer.get(player);
+    }
+
+    public Question getQuestion() {
+        return currentQuestion;
+    }
+
+    private void prepareNextQuestion() {
+        questionManager.removeAnsweredQuestion();
+        currentQuestion = questionManager.getNextQuestion();
+    }
+
+    public void executeActionsBeforeNextQuestion() {
+        resetAllAnswerData();
+        prepareNextQuestion();
+    }
+
+    public void executeActionsBeforeNextRound() {
+        resetAllRoundData();
+        prepareNextQuestion();
+    }
+}
