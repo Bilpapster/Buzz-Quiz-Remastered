c579da815d27f070ff504067c18ca613f2a1e3e0
Vasileios Papastergios <papastva@csd.auth.gr>
2020-12-26 23:05:39 +0200

    Removed all non-logic-related methods (reading from the console and printing to it). The purely logic-related methods remain intact. Removed unused import statements.

--- src/com/RoundI.java
+++ src/com/RoundI.java
@@ -1,7 +1,5 @@
 package com;
 
-import java.util.List;
-
 /**
  * An interface, representing a round in our game.
  */
@@ -20,23 +18,6 @@ public interface RoundI {
      * @return true if the round is over, else false.
      */
     Boolean isOver();
-
-    /**
-     * Getter for the players structure of the round.
-     *
-     * @return an ArrayList containing the the players involved in the round.
-     */
-    List<Player> getPlayers();
-
-    /**
-     * Lets the player(s) know the question (category, question body and available answers).
-     */
-    void askQuestion();
-
-    /**
-     * Collects the answers given by players involved in the round.
-     */
-    void readAnswers();
 
     /**
      * Takes care of updating the players' score.
