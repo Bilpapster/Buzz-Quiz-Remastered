7876888d5b8095a1efa91d106d9c488fdc1b19fd
Vasileios Papastergios <papastva@csd.auth.gr>
2020-12-27 19:07:20 +0200

    Added one background image for every QuestionType.

---
+++ src/GUI/BackgroundImagedPanel.java
@@ -0,0 +1,27 @@
+package GUI;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class BackgroundImagedPanel extends JPanel {
+
+    private Image backgroundImage;
+
+    public BackgroundImagedPanel(String backgroundImage) {
+        this(new ImageIcon(backgroundImage).getImage());
+    }
+
+    public BackgroundImagedPanel(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+    public void setBackgroundImage(ImageIcon backgroundImage) {
+        this.backgroundImage = backgroundImage.getImage();
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawImage(backgroundImage, 0, 0, null);
+    }
+
+}
