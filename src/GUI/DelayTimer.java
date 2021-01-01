89778505c4675496a574baded42541b83a8fead8
Vasileios Papastergios <papastva@csd.auth.gr>
2021-01-01 11:52:54 +0200

    Added JavaDoc.

--- src/GUI/DelayTimer.java
+++ src/GUI/DelayTimer.java
@@ -4,23 +4,38 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+/**
+ * An abstract class that represents a delay timer in our game. It can be used in order to delay the execution of
+ * a specific block of code.
+ *
+ * @author Vasileios Papastergios
+ */
 public abstract class DelayTimer {
     private Timer timer;
 
+    /**
+     * Constructs a <code>DelayTimer</code> object with given duration.
+     *
+     * @param durationInMillis the duration to pause execution between actions before and after.
+     */
     public DelayTimer(int durationInMillis) {
-        timer = new Timer(durationInMillis, new ActionListener() {
+        timer = new Timer(durationInMillis, e -> {
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                ((javax.swing.Timer) e.getSource()).stop();
-                executeActionsAfterDelay();
-            }
+            ((Timer) e.getSource()).stop();
+            executeActionsAfterDelay();
         });
         timer.start();
         executeActionsBeforeDelay();
     }
 
+    /**
+     * Executes all actions needed before pausing execution.
+     */
     protected abstract void executeActionsBeforeDelay();
+
+    /**
+     * Executes all actions needed right after the pause.
+     */
     protected abstract void executeActionsAfterDelay();
 }
--- src/GUI/KeyboardSet.java
+++ src/GUI/KeyboardSet.java
@@ -1,5 +1,11 @@
 package GUI;
 
+/**
+ * A simple enumeration that represents a keyboard answering set. The set contains the keyboard characters that are
+ * accepted for the players in order to declare their choice among the available in each question.
+ *
+ * @author Vasileios Papastergios
+ */
 public enum KeyboardSet {
     _Q_W_A_S, _O_P_K_L, _A_B_C_D, _1_2_3_4, _A_S_Z_X, _K_L_N_M;
 
