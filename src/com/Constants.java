2d8d04f7a7d9b92ca7193604998d1b5c3dada585
fmalakis <xan.klem.30@gmail.com>
2021-01-13 00:29:57 +0200

    Added missing filepath in SOUND_PATHS

--- src/com/Constants.java
+++ src/com/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     public static String[] SOUND_PATHS = {
             "src/resources/SoundClips/correct_answer.wav",
             "src/resources/SoundClips/wrong_answer.wav",
-            "src/resources/SoundClips/main_menu_theme.wav"
+            "src/resources/SoundClips/main_menu_theme.wav",
+            "src/resources/SoundClips/during_game_theme.wav"
     } ;
 }
