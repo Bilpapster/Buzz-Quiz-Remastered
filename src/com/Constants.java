d2684d58753191740dd284db5e3d84012587b60c
fmalakis <xan.klem.30@gmail.com>
2021-01-12 19:10:31 +0200

    Updated Constants.java with new files

---
+++ src/com/Constants.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Constants {
+
+    public static String[] SOUND_PATHS = {
+            "src/resources/SoundClips/correct_answer.wav",
+            "src/resources/SoundClips/wrong_answer.wav",
+            "src/resources/SoundClips/main_menu_theme.wav"
+    } ;
+}
