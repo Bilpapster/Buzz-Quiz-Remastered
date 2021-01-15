7753ccb3a12382bf84929a941def0a18de95caf1
fmalakis <xan.klem.30@gmail.com>
2021-01-15 16:30:15 +0200

    Made SoundManager's fields static in order to eliminate the need to pass the original object around JFrames, added toggleMute for Music and SFX

--- src/com/Sound/SoundManager.java
+++ src/com/Sound/SoundManager.java
@@ -7,16 +7,20 @@ import java.util.HashMap;
 
 public class SoundManager {
 
-    private HashMap<String, SoundClip> clips;
-    private Constants constants;
+    private static boolean isMutedMusic = false;
+    private static boolean isMutedSFX = false;
+    private static HashMap<String, SoundClip> clips;
+    private static Constants constants;
+    private static float[] floatControlValuesSFX, floatControlValuesTHEME;
 
     public SoundManager() {
         constants = new Constants();
         createSoundMap();
+        createFloatValues();
     }
 
 
-    private void createSoundMap() {
+    private static void createSoundMap() {
 
         clips = new HashMap<>();
 
@@ -37,31 +41,84 @@ public class SoundManager {
 
     }
 
-    public void adjustSound(float soundLevel, SoundType soundType) {
+    private void createFloatValues() {
+        FloatControl floatControlSFX = getFloatControlSFX();
+        FloatControl floatControlTHEME = getFloatControlTHEME();
+//        floatControlValues = new ArrayList<>();
+//        floatControlValues.add(floatControl.getMinimum());
+//        floatControlValues.add(floatControl.getMaximum());
+//        floatControlValues.add(floatControl.getValue());
+
+        floatControlValuesSFX = new float[]{floatControlSFX.getMinimum(), floatControlSFX.getMaximum(), floatControlSFX.getValue()};
+        floatControlValuesTHEME = new float[]{floatControlTHEME.getMinimum(), floatControlTHEME.getMaximum(), floatControlTHEME.getValue()};
+    }
+
+    public static void adjustSound(float soundLevel, SoundType soundType) {
         for(String clipName: clips.keySet()) {
             SoundClip clip = clips.get(clipName);
             if (clip.getSoundType() == soundType)
                 clip.setSoundLevel(soundLevel);
+            if (soundType == SoundType.CLIP)
+                floatControlValuesSFX[2] = soundLevel;
+            else
+                floatControlValuesTHEME[2] = soundLevel;
         }
     }
 
-    public void playClip(String name) {
+    public static void toggleMusicMute() {
+        isMutedMusic = !isMutedMusic;
+        for(String clipName: clips.keySet()) {
+            SoundClip clip = clips.get(clipName);
+            if (clip.getSoundType() == SoundType.THEME)
+                clip.toggleMute();
+        }
+    }
+
+    public static void toggleSFXMute() {
+        isMutedSFX = !isMutedSFX;
+        for(String clipName: clips.keySet()) {
+            SoundClip clip = clips.get(clipName);
+            if (clip.getSoundType() == SoundType.CLIP)
+                clip.toggleMute();
+        }
+    }
+
+
+    public static float[] getFloatControlValuesSFX() {
+        return floatControlValuesSFX;
+    }
+
+    public static float[] getFloatControlValuesTHEME() {
+        return floatControlValuesTHEME;
+    }
+
+    public static void playClip(String name) {
         if (clips.containsKey(name))
             clips.get(name).play();
         else
             System.out.println("Clip doesn't exist...");
     }
 
-    public void stopClip(String name) {
+    public static void stopClip(String name) {
         if (clips.containsKey(name))
             clips.get(name).stop();
         else
             System.out.println("Clip doesn't exist...");
     }
 
-    public FloatControl getFloatControl() {
+    public static FloatControl getFloatControlSFX() {
         return clips.get("main_menu_theme").getFloatControl();
     }
 
+    public static FloatControl getFloatControlTHEME() {
+        return clips.get("correct_answer").getFloatControl();
+    }
 
+    public static boolean isIsMutedMusic() {
+        return isMutedMusic;
+    }
+
+    public static boolean isIsMutedSFX() {
+        return isMutedSFX;
+    }
 }
